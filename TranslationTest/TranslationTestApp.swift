0d11f84205b49cdc0d2ef869c9f3f8be6633cb3d
Etienne Negro <etienne.wojahn@me.com>
2020-09-25 16:06:11 +0200

    Init project

---
+++ TranslationTest/TranslationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationTestApp.swift
+//  TranslationTest
+//
+//  Created by Etienne Negro on 25.09.20.
+//
+
+import SwiftUI
+
+@main
+struct TranslationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
